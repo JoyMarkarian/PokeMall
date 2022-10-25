c851eb633f8b84dc35cf7f6962275fadc3ed8fef
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2022-10-25 16:45:32 +0200

    ajout data du projet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import Home from "@pages/Home";
+import pokemons from "@services/pokemons";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Home pokemons={pokemons} />
     </div>
   );
 }
