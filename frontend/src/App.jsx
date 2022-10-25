d623524c71572217049e084a82aab9f53f822770
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2022-10-25 17:04:09 +0200

    ajout des composants header et footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@ import AllProducts from "@pages/AllProducts";
 import Basket from "@pages/Basket";
 import Home from "@pages/Home";
 import OneProduct from "@pages/OneProduct";
+
+import Footer from "@components/Footer";
+import Header from "@components/Header";
+
 import pokemons from "@services/pokemons";
 
 import "./App.css";
@@ -9,10 +13,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home pokemons={pokemons} />
       <AllProducts />
       <OneProduct />
       <Basket />
+      <Footer />
     </div>
   );
 }
