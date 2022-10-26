c6f91901d9c21e39d5073d9f9f4bb4ae9ace654b
guillaume wernert <guil.wernert@gmail.com>
2022-10-26 14:18:04 +0200

    ajout des images statiques

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,24 +1,63 @@
 export default function Home() {
   return (
     <div>
-      <div className="News">
+      <div className="ImageBck">
+        <h1 className="CollectAll">COLLECT THEM ALL</h1>
+      </div>
+      <div className="NewsHome">
         <h1>NEWS</h1>
-          <div className="ImageHomeNews">
-            <img src="" alt="" className="HomeNews" />
-            <img src="" alt="" className="HomeNews" />
-            <img src="" alt="" className="HomeNews" />
-            <img src="" alt="" className="HomeNews" />
-          </div>
+        <div className="ImageHomeNews">
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+            alt=""
+            className="HomeNews1"
+          />
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg"
+            alt=""
+            className="HomeNews2"
+          />
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg"
+            alt=""
+            className="HomeNews3"
+          />
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg"
+            alt=""
+            className="HomeNews4"
+          />
+        </div>
       </div>
       <div className="BestSellers">
         <h1>BEST SELLERS</h1>
-          <div className="CarouselBest">
-            <img src="" alt="" className="CarouselBest" />
-            <img src="" alt="" className="CarouselBest" />
-            <img src="" alt="" className="CarouselBest" />
-            <img src="" alt="" className="CarouselBest" />
-            <img src="" alt="" className="CarouselBest" />
-          </div>
+        <div className="CarouselBest">
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/5.svg"
+            alt=""
+            className="CarouselBest1"
+          />
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/6.svg"
+            alt=""
+            className="CarouselBest2"
+          />
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/7.svg"
+            alt=""
+            className="CarouselBest3"
+          />
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/8.svg"
+            alt=""
+            className="CarouselBest4"
+          />
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/9.svg"
+            alt=""
+            className="CarouselBest5"
+          />
+        </div>
       </div>
     </div>
   );
