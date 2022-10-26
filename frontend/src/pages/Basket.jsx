0a99a64f5589a36c78229cadcf4226f47a5207a0
Tormo <sylvaain11@hotmail.fr>
2022-10-26 14:21:35 +0200

    premier commit test, ajout html en dur

--- frontend/src/pages/Basket.jsx
+++ frontend/src/pages/Basket.jsx
@@ -1,5 +1,20 @@
 function Basket() {
-  return <div>Basket</div>;
+  return (
+    <div>
+      <h1 className="text-center">Basket</h1>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+        alt="pokemon"
+      />
+      <h2>Carapuce </h2>
+      <p>
+        Quantité : <button type="button">-1+</button>
+      </p>
+      <h2>Prix</h2>
+      <p>5,99€</p>
+      <button type="button">Supprimer</button>
+    </div>
+  );
 }
 
 export default Basket;
