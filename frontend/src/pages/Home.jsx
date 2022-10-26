5d219d65a3080cdfc03762b7159f9ca93858ab1b
guillaume wernert <guil.wernert@gmail.com>
2022-10-26 11:07:04 +0200

    1er commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,25 @@
 export default function Home() {
   return (
     <div>
-      <h1>home</h1>
+      <div className="News">
+        <h1>NEWS</h1>
+          <div className="ImageHomeNews">
+            <img src="" alt="" className="HomeNews" />
+            <img src="" alt="" className="HomeNews" />
+            <img src="" alt="" className="HomeNews" />
+            <img src="" alt="" className="HomeNews" />
+          </div>
+      </div>
+      <div className="BestSellers">
+        <h1>BEST SELLERS</h1>
+          <div className="CarouselBest">
+            <img src="" alt="" className="CarouselBest" />
+            <img src="" alt="" className="CarouselBest" />
+            <img src="" alt="" className="CarouselBest" />
+            <img src="" alt="" className="CarouselBest" />
+            <img src="" alt="" className="CarouselBest" />
+          </div>
+      </div>
     </div>
   );
 }
