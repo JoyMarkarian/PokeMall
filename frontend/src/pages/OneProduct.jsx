8255f069e68f8a3de0b872897a2f2d2fb962cf3c
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2022-10-28 12:05:26 +0200

    ajout des fonts, de la couleur de fond, redimensionnement des types et abilities

--- frontend/src/pages/OneProduct.jsx
+++ frontend/src/pages/OneProduct.jsx
@@ -1,6 +1,6 @@
 function OneProduct() {
   return (
-    <div className="container">
+    <div className="container divPrincipale">
       <div className="row">
         <div className="col-6 text-center">
           <img
@@ -15,30 +15,37 @@ function OneProduct() {
             <p className="fw-bold text-center importantText">ID: 001</p>
           </div>
           <div className="d-flex flex-column align-items-center">
-            <p className="fw-bold">price: 5.99$</p>
-            <button type="button" className="btn btn-warning shadow">
+            <p className="fw-bold textRegular">price: 5.99$</p>
+            <button
+              type="button"
+              className="btn btn-warning shadow textRegular"
+            >
               add to cart
             </button>
           </div>
         </div>
       </div>
       <div className="row">
-        <div className="mt-4 col-xs-12 col-sm-12 col-md-6 col-lg-6">
+        <div className="mt-4 col-xs-12 col-sm-12 col-md-6 col-lg-6 text-center">
           <h2 className="text-center importantText">Type</h2>
-          <ul className=" list-group list-group-horizontal">
-            <li className="list-group-item text-center w-50 bg-success text-white">
-              primary: grass
+          <ul className=" list-group list-group-horizontal justify-content-center">
+            <li className="list-group-item text-center w-25 bg-success text-white me-1 textRegular">
+              Grass
             </li>
-            <li className="list-group-item text-center w-50 bg-warning">
-              secondary: poison
+            <li className="list-group-item text-center w-25 bg-warning ms-1 textRegular">
+              Poison
             </li>
           </ul>
         </div>
         <div className="mt-4 col-xs-12 col-sm-12 col-md-6 col-lg-6">
           <h2 className="text-center importantText">Abilities</h2>
-          <ul className="list-group list-group-horizontal">
-            <li className="list-group-item text-center w-50">overgrow</li>
-            <li className="list-group-item text-center w-50">chlorophyll</li>
+          <ul className="list-group list-group-horizontal justify-content-center">
+            <li className="list-group-item w-25 p-0 text-center pt-2 pb-2 bg-success text-white me-1 textRegular">
+              Overgrow
+            </li>
+            <li className="list-group-item w-25 p-0 text-center pt-2 pb-2 bg-success text-white ms-1 textRegular">
+              Chlorophyll
+            </li>
           </ul>
         </div>
       </div>
@@ -47,39 +54,39 @@ function OneProduct() {
         <div className="row mt-4">
           <div className="d-flex flex-column align-items-center col-4">
             <div className="rounded-circle bg-info text-white stats">
-              <p className="text-center lh-lg">49</p>
+              <p className="text-center lh-lg textRegular">49</p>
             </div>
-            <h5 className="text-center">attack</h5>
+            <h5 className="text-center textRegular">Attack</h5>
           </div>
           <div className="d-flex flex-column align-items-center col-4">
             <div className="rounded-circle bg-info text-white stats">
-              <p className="text-center lh-lg">49</p>
+              <p className="text-center lh-lg textRegular">49</p>
             </div>
-            <h5 className="text-center">defense</h5>
+            <h5 className="text-center textRegular">Defense</h5>
           </div>
           <div className="d-flex flex-column align-items-center col-4">
             <div className="rounded-circle bg-info text-white stats">
-              <p className="text-center lh-lg">65</p>
+              <p className="text-center lh-lg textRegular">65</p>
             </div>
-            <h5 className="text-center">special attack</h5>
+            <h5 className="text-center textRegular">Special Attack</h5>
           </div>
           <div className="d-flex flex-column align-items-center col-4 mt-4">
             <div className="rounded-circle bg-info text-white stats">
-              <p className="text-center lh-lg">65</p>
+              <p className="text-center lh-lg textRegular">65</p>
             </div>
-            <h5 className="text-center">special defense</h5>
+            <h5 className="text-center textRegular">Special Defense</h5>
           </div>
           <div className="d-flex flex-column align-items-center col-4 mt-4">
             <div className="rounded-circle bg-info text-white stats">
-              <p className="text-center lh-lg">45</p>
+              <p className="text-center lh-lg textRegular">45</p>
             </div>
-            <h5 className="text-center">speed</h5>
+            <h5 className="text-center textRegular">Speed</h5>
           </div>
           <div className="d-flex flex-column align-items-center col-4 mt-4">
             <div className="rounded-circle bg-info text-white stats">
-              <p className="text-center lh-lg">45</p>
+              <p className="text-center lh-lg textRegular">45</p>
             </div>
-            <h5 className="text-center">Life</h5>
+            <h5 className="text-center textRegular">Life</h5>
           </div>
         </div>
       </div>
@@ -87,8 +94,8 @@ function OneProduct() {
         <h2 className="text-center importantText mt-4">Reviews</h2>
         <div className="row mt-4">
           <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 border-end border-2">
-            <p className="fw-bold text-center">Lorem ipsum</p>
-            <p>
+            <p className="fw-bold text-center textRegular">Lorem ipsum</p>
+            <p className="textRegular">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
               optio exercitationem laudantium obcaecati, eum saepe distinctio
               minus modi ullam tenetur nobis inventore ipsam, consequuntur error
@@ -96,8 +103,8 @@ function OneProduct() {
             </p>
           </div>
           <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 border-end border-2">
-            <p className="fw-bold text-center">Lorem ipsum</p>
-            <p>
+            <p className="fw-bold text-center textRegular">Lorem ipsum</p>
+            <p className="textRegular">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
               earum optio exercitationem laudantium obcaecati, eum saepe
               distinctio minus modi ullam tenetur nobis inventore ipsam,
@@ -105,8 +112,8 @@ function OneProduct() {
             </p>
           </div>
           <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-            <p className="fw-bold text-center">Lorem ipsum</p>
-            <p>
+            <p className="fw-bold text-center textRegular">Lorem ipsum</p>
+            <p className="textRegular">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
               earum optio exercitationem laudantium obcaecati, eum saepe
               distinctio minus modi ullam tenetur nobis inventore ipsam,
