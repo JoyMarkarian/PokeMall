83a005eb9b9a889df8303e667826ae185fcd63ca
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2022-10-30 18:23:44 +0100

    ajout des props avec le premier pokemon comme données de référence

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,22 @@ function App() {
       <Header />
       <Home pokemons={pokemons} />
       <AllProducts />
-      <OneProduct />
+      <OneProduct
+        index={pokemons[0].pokedex_index}
+        name={pokemons[0].name}
+        image={pokemons[0].url}
+        life={pokemons[0].hp}
+        attack={pokemons[0].stats.attack}
+        defense={pokemons[0].stats.defense}
+        specialAttack={pokemons[0].stats.special_attack}
+        specialDefense={pokemons[0].stats.special_defence}
+        speed={pokemons[0].stats.speed}
+        primaryType={pokemons[0].type.primary_type}
+        secondaryType={pokemons[0].type.secondary_type}
+        price={pokemons[0].price}
+        firstAbility={pokemons[0].ability.first_ability}
+        secondAbility={pokemons[0].ability.seconde_ability}
+      />
       <Basket />
       <Footer />
     </div>
--- frontend/src/pages/OneProduct.jsx
+++ frontend/src/pages/OneProduct.jsx
@@ -1,21 +1,33 @@
-function OneProduct() {
+function OneProduct(props) {
+  const {
+    image,
+    name,
+    index,
+    price,
+    primaryType,
+    secondaryType,
+    firstAbility,
+    secondAbility,
+    attack,
+    defense,
+    specialAttack,
+    specialDefense,
+    speed,
+    life,
+  } = props;
   return (
-    <div className="container divPrincipale">
+    <div className="container-fluid divPrincipale">
       <div className="row pt-5">
         <div className="col-6 text-center">
-          <img
-            alt="pokemon"
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-            className="img-fluid pulse"
-          />
+          <img alt="pokemon" src={image} className="img-fluid pulse" />
         </div>
         <div className="col-6 d-flex flex-column justify-content-between">
           <div className="d-flex flex-column justify-content-around">
-            <h1 className="text-center importantText">Bulbasaur</h1>
-            <p className="fw-bold text-center importantText">ID: 001</p>
+            <h1 className="text-center importantText">{name}</h1>
+            <p className="fw-bold text-center importantText">ID: 00{index}</p>
           </div>
           <div className="d-flex flex-column align-items-center">
-            <p className="fw-bold textRegular">price: 5.99$</p>
+            <p className="fw-bold textRegular">price: {price}$</p>
             <button
               type="button"
               className="btn btn-warning shadow textRegular"
@@ -30,10 +42,10 @@ function OneProduct() {
           <h2 className="text-center importantText">Type</h2>
           <ul className=" list-group list-group-horizontal justify-content-center">
             <li className="list-group-item text-center w-25 bg-success text-white me-1 textRegular">
-              Grass
+              {primaryType}
             </li>
             <li className="list-group-item text-center w-25 bg-warning ms-1 textRegular">
-              Poison
+              {secondaryType}
             </li>
           </ul>
         </div>
@@ -41,10 +53,10 @@ function OneProduct() {
           <h2 className="text-center importantText">Abilities</h2>
           <ul className="list-group list-group-horizontal justify-content-center">
             <li className="list-group-item w-25 p-0 text-center pt-2 pb-2 bg-success text-white me-1 textRegular">
-              Overgrow
+              {firstAbility}
             </li>
             <li className="list-group-item w-25 p-0 text-center pt-2 pb-2 bg-success text-white ms-1 textRegular">
-              Chlorophyll
+              {secondAbility}
             </li>
           </ul>
         </div>
@@ -54,37 +66,37 @@ function OneProduct() {
         <div className="row mt-4">
           <div className="d-flex flex-column align-items-center col-4">
             <div className="rounded-circle bg-info text-white stats">
-              <p className="text-center lh-lg textRegular">49</p>
+              <p className="text-center lh-lg textRegular">{attack}</p>
             </div>
             <h5 className="text-center textRegular">Attack</h5>
           </div>
           <div className="d-flex flex-column align-items-center col-4">
             <div className="rounded-circle bg-info text-white stats">
-              <p className="text-center lh-lg textRegular">49</p>
+              <p className="text-center lh-lg textRegular">{defense}</p>
             </div>
             <h5 className="text-center textRegular">Defense</h5>
           </div>
           <div className="d-flex flex-column align-items-center col-4">
             <div className="rounded-circle bg-info text-white stats">
-              <p className="text-center lh-lg textRegular">65</p>
+              <p className="text-center lh-lg textRegular">{specialAttack}</p>
             </div>
             <h5 className="text-center textRegular">Special Attack</h5>
           </div>
           <div className="d-flex flex-column align-items-center col-4 mt-4">
             <div className="rounded-circle bg-info text-white stats">
-              <p className="text-center lh-lg textRegular">65</p>
+              <p className="text-center lh-lg textRegular">{specialDefense}</p>
             </div>
             <h5 className="text-center textRegular">Special Defense</h5>
           </div>
           <div className="d-flex flex-column align-items-center col-4 mt-4">
             <div className="rounded-circle bg-info text-white stats">
-              <p className="text-center lh-lg textRegular">45</p>
+              <p className="text-center lh-lg textRegular">{speed}</p>
             </div>
             <h5 className="text-center textRegular">Speed</h5>
           </div>
           <div className="d-flex flex-column align-items-center col-4 mt-4">
             <div className="rounded-circle bg-info text-white stats">
-              <p className="text-center lh-lg textRegular">45</p>
+              <p className="text-center lh-lg textRegular">{life}</p>
             </div>
             <h5 className="text-center textRegular">Life</h5>
           </div>
