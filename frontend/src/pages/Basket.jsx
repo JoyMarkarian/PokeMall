222e4a116b5846425866d64f29ab48714967cfb9
Tormo <sylvaain11@hotmail.fr>
2022-11-03 16:09:21 +0100

    vue dekstop end

--- frontend/src/pages/Basket.jsx
+++ frontend/src/pages/Basket.jsx
@@ -6,7 +6,7 @@ function Basket() {
       <div className="container md">
         <h1 className="text-center">Your Basket</h1>
         <div className="basket-dekstop">
-          <div className="row">
+          <div className="row w-100">
             <div className="img-basket col-6 my-3">
               <img
                 src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
@@ -15,8 +15,9 @@ function Basket() {
               />
             </div>
             <div className="basket-top col-6 my-3 md">
-              <h2>Bulbizar</h2>
-              <div className="product-basket form-floating w-75 d-inline-block justify-content-center md">
+              <h2 className="my-5">Bulbizar</h2>
+              <div className="product-basket w-30 d-inline-block justify-content-center md">
+                <label htmlFor="floatingSelect">Quantity</label>
                 <select className="quantity form-select" id="quantitySelect">
                   <option value="1">
                     <b>1</b>
@@ -49,88 +50,90 @@ function Basket() {
                     <b>10</b>
                   </option>
                 </select>
-                <label htmlFor="floatingSelect">
-                  <b>Quantity</b>
-                </label>
               </div>
-              <button type="button" className="btn2 btn-delete-product my-2">
+              <button type="button" className="btn-delete btn btn-link my-2">
                 Delete
               </button>
               <h3 className="my-3">Price : 10$</h3>
             </div>
           </div>
-          <div className="summary-order container card md">
-            <br />
-            <h1 className="text-center">Summary of orders</h1>
-            <div className="d-flex mb-3 p-2 w-100 justify-content-between my-3">
-              <h3>Total items</h3>
-              <h4>10$</h4>
+          <div className="card w-50">
+            <div className="summary-order container md">
+              <br />
+              <h1 className="text-center">Summary of orders</h1>
+              <div className="d-flex mb-3 p-2 w-100 justify-content-between my-3">
+                <h3>Total items</h3>
+                <h4>10$</h4>
+              </div>
+              <div className="d-flex mb-3 p-2 w-100 justify-content-between">
+                <p>Including TVA</p>
+                <p>0.99$</p>
+              </div>
+              <div className="d-flex mb-3 p-2 w-100 justify-content-between">
+                <h3>Delivery cost</h3>
+                <h4>FREE</h4>
+              </div>
+              <div className="d-flex justify-content-center">
+                <button
+                  type="button"
+                  className="valide-basket btn btn-success btn-lg mb-2"
+                >
+                  Validate my order
+                </button>
+              </div>
             </div>
-            <div className="d-flex mb-3 p-2 w-100 justify-content-between">
-              <p>Including TVA</p>
-              <p>0.99$</p>
+
+            <div className="my-2">
+              <h1 className="m-3 d-flex justify-content-center">Delivery</h1>
+              <div className="delivery-basket form-check m-2">
+                <input
+                  className="form-check-input shadow-none"
+                  type="radio"
+                  name="flexRadioDefault"
+                  id="flexRadioDefault1"
+                />
+                <label
+                  className="form-check-label float-start"
+                  htmlFor="flexRadioDefault1"
+                >
+                  Home delivery by Team Rocket
+                </label>
+              </div>
+              <div className="form-check m-2">
+                <input
+                  className="form-check-input shadow-none"
+                  type="radio"
+                  name="flexRadioDefault"
+                  id="flexRadioDefault1"
+                />
+                <label
+                  className="form-check-label float-start"
+                  htmlFor="flexRadioDefault1"
+                >
+                  Home delivery by Teacher Chen
+                </label>
+              </div>
+              <div className="form-check m-2">
+                <input
+                  className="form-check-input shadow-none"
+                  type="radio"
+                  name="flexRadioDefault"
+                  id="flexRadioDefault1"
+                />
+                <label
+                  className="form-check-label float-start"
+                  htmlFor="flexRadioDefault1"
+                >
+                  Home delivery by Sacha
+                </label>
+              </div>
             </div>
-            <div className="d-flex mb-3 p-2 w-100 justify-content-between">
-              <h3>Delivery cost</h3>
-              <h4>FREE</h4>
+            <div className="valide-basket-mobile card m-3">
+              <button type="button" className="btn btn-success btn-lg">
+                Validate my order
+              </button>
             </div>
-            <button
-              type="button"
-              className="valide-basket btn btn-success btn-lg"
-            >
-              Validate my order
-            </button>
           </div>
-        </div>
-        <div className="card my-2">
-          <h1 className="m-3">Delivery</h1>
-          <div className="form-check m-2">
-            <input
-              className="form-check-input"
-              type="radio"
-              name="flexRadioDefault"
-              id="flexRadioDefault1"
-            />
-            <label
-              className="form-check-label float-start"
-              htmlFor="flexRadioDefault1"
-            >
-              Home delivery by Team Rocket
-            </label>
-          </div>
-          <div className="form-check m-2">
-            <input
-              className="form-check-input"
-              type="radio"
-              name="flexRadioDefault"
-              id="flexRadioDefault1"
-            />
-            <label
-              className="form-check-label float-start"
-              htmlFor="flexRadioDefault1"
-            >
-              Home delivery by Teacher Chen
-            </label>
-          </div>
-          <div className="form-check m-2">
-            <input
-              className="form-check-input"
-              type="radio"
-              name="flexRadioDefault"
-              id="flexRadioDefault1"
-            />
-            <label
-              className="form-check-label float-start"
-              htmlFor="flexRadioDefault1"
-            >
-              Home delivery by Sacha
-            </label>
-          </div>
-        </div>
-        <div className="valide-basket-mobile card m-3">
-          <button type="button" className="btn btn-success btn-lg">
-            Validate my order
-          </button>
         </div>
       </div>
     </div>
