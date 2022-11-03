9a30d28e9a7400b4709893707af2c09be994193b
vassili45 <vassili.papaodpoulos45@gmail.com>
2022-11-03 14:51:35 +0100

    add fils Navbar

---
+++ frontend/src/components/Navbar/NavbarWindow.jsx
@@ -0,0 +1,11 @@
+export default function NavbarWindow() {
+  return (
+    <nav className="navbarFullscreen">
+      <div className="d-flex flex-column flex-md-row">
+        <div className="p-2 flex-fill">Home</div>
+        <div className="p-2 flex-fill">Products</div>
+        <div className="p-2 flex-fill">Contact</div>
+      </div>
+    </nav>
+  );
+}
