728fb8997b2ee1a92b3e7c064f8dd110c3d9ca9c
vassili45 <vassili.papaodpoulos45@gmail.com>
2022-11-09 10:04:56 +0100

    add one product on systemeon nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
-import React,{ useState } from "react";
-
+import React, { useState } from "react";
 
 import AllProducts from "@pages/AllProducts";
 import Basket from "@pages/Basket";
@@ -14,34 +13,35 @@ import pokemons from "./services/pokemons";
 import "./App.css";
 
 function App() {
-
-  const [page, setPage] = useState({ path: "Home", index: null });
+  const [page, setPage] = useState({ path: "one Product", index: 1 });
   const [id, setId] = React.useState(0);
 
   return (
     <div className="App">
       <Header setPage={setPage} />
-      {page.path === "Home" ? <Home pokemons={pokemons} /> : ""}
-      {page.path === "Products" ? <AllProducts /> : ""}
-      {page.path === "Basket" ? <Basket /> : ""}
-      <OneProduct
-        index={pokemons[id].pokedex_index}
-        name={pokemons[id].name}
-        image={pokemons[id].url}
-        life={pokemons[id].hp}
-        attack={pokemons[id].stats.attack}
-        defense={pokemons[id].stats.defense}
-        specialAttack={pokemons[id].stats.special_attack}
-        specialDefense={pokemons[id].stats.special_defence}
-        speed={pokemons[id].stats.speed}
-        primaryType={pokemons[id].type.primary_type}
-        secondaryType={pokemons[id].type.secondary_type}
-        price={pokemons[id].price}
-        firstAbility={pokemons[id].ability.first_ability}
-        secondAbility={pokemons[id].ability.seconde_ability}
-        id={id}
-        setId={setId}
-      />
+      {page.path === "Home" && <Home pokemons={pokemons} />}
+      {page.path === "Products" && <AllProducts />}
+      {page.path === "Basket" && <Basket />}
+      {page.path === "one Product" && (
+        <OneProduct
+          index={pokemons[page.index].pokedex_index}
+          name={pokemons[page.index].name}
+          image={pokemons[page.index].url}
+          life={pokemons[page.index].hp}
+          attack={pokemons[page.index].stats.attack}
+          defense={pokemons[page.index].stats.defense}
+          specialAttack={pokemons[page.index].stats.special_attack}
+          specialDefense={pokemons[page.index].stats.special_defence}
+          speed={pokemons[page.index].stats.speed}
+          primaryType={pokemons[page.index].type.primary_type}
+          secondaryType={pokemons[page.index].type.secondary_type}
+          price={pokemons[page.index].price}
+          firstAbility={pokemons[page.index].ability.first_ability}
+          secondAbility={pokemons[page.index].ability.seconde_ability}
+          id={id}
+          setId={setId}
+        />
+      )}
       {page.path === "Contact" ? <Footer /> : ""}
     </div>
   );
