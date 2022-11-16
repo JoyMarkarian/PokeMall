56f5f57d8bdb033cbda76ea50af089ab9f90ae11
JoyMarkarian <jmarkarian@hotmail.fr>
2022-11-16 15:45:02 +0100

    Hotfix Form

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -27,8 +27,7 @@ function Form() {
 
   return (
     <div className="formBG m-0 pt-4">
-      <form className="container" ref={form} onSubmit={sendEmail}>
-      <form className="container">
+      <form className="container" id="form" ref={form} onSubmit={sendEmail}>
         <h1 className="text-center fs-1 mb-4 importantText">Contact</h1>
         <div className="container row m-0">
           <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 d-flex flex-column align-items-center align-items-sm-center align-items-md-end align-items-lg-end align-items-xl-end">
@@ -50,15 +49,18 @@ function Form() {
             />
             <input
               type="email"
-              name="Email"           
+              name="Email"
               placeholder="Email"
               required
-
               className="rounded w-75 thirdInput textRegular"
             />
           </div>
           <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 d-flex flex-column align-items-center align-items-sm-center align-items-md-start align-items-lg-start align-items-xl-start">
-            <textarea name="Message" placeholder="Message" className="rounded w-75 h-100" />
+            <textarea
+              name="Message"
+              placeholder="Message"
+              className="rounded w-75 h-100"
+            />
           </div>
           <div className=" col -12 text-end mt-2">
             <button
