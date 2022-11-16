ac16da8fe97004791060348a3f9d10ce28e5ad6b
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2022-11-16 14:08:44 +0100

    setting emailjs service

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,13 +1,39 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 function Form() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_05bptfr",
+        "template_b1b8eld",
+        form.current,
+        `${import.meta.env.VITE_EMAILJS_PUBLIC_KEY}`
+      )
+      .then(
+        (result) => {
+          console.warn(result.text);
+        },
+        (error) => {
+          console.error(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
     <div className="formBG m-0 pt-4">
-      <form className="container">
+      <form className="container" ref={form} onSubmit={sendEmail}>
         <h1 className="text-center fs-1 mb-4 importantText">contact</h1>
         <div className="container row m-0">
           <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 d-flex flex-column align-items-center align-items-sm-center align-items-md-end align-items-lg-end align-items-xl-end">
             <input
               type="text"
-              name="name"
+              name="Firstname"
               id="nameinput"
               placeholder="firstname"
               required
@@ -15,7 +41,7 @@ function Form() {
             />
             <input
               type="text"
-              name="name"
+              name="Lastname"
               id="nameinput"
               placeholder="lastname"
               required
@@ -23,17 +49,19 @@ function Form() {
             />
             <input
               type="email"
+              name="Email"
               placeholder="email"
               className="rounded w-75 thirdInput textRegular"
             />
           </div>
           <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 d-flex flex-column align-items-center align-items-sm-center align-items-md-start align-items-lg-start align-items-xl-start">
-            <textarea className="rounded w-75 h-100" />
+            <textarea name="Message" className="rounded w-75 h-100" />
           </div>
           <div className=" col -12 text-end mt-2">
             <button
               type="submit"
               className="btn btn-warning mb-2 contactButton textRegular"
+              onClick={() => sendEmail}
             >
               send
             </button>
