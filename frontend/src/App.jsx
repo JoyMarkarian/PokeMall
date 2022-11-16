fa1475b7e3b3e6717555f84713fabc082830761e
vassili45 <vassili.papaodpoulos45@gmail.com>
2022-11-16 16:56:13 +0100

    add crete state form panier and implement add and delete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,19 +15,37 @@ import "./App.css";
 
 function App() {
   const [search, setSearch] = useState("");
+  const [panier, setPanier] = useState([]);
 
   const handleSearch = (value) => {
     setSearch(value);
   };
 
+  const handlePanier = (addCard) => {
+    const newPanier = panier;
+    newPanier.push(addCard);
+    setPanier(newPanier);
+  };
+  const handleDeletPanier = (toDelete) => {
+    const newPanier = panier.filter((pokemon) => pokemon.id !== toDelete.id);
+    setPanier(newPanier);
+  };
   return (
     <Router>
       <Header search={search} handleSearch={handleSearch} />
       <Routes>
         <Route path="/" element={<Home pokemons={pokemons} />} />
         <Route path="/products" element={<AllProducts search={search} />} />
-        <Route path="/cart" element={<Basket />} />
-        <Route path="/products/:id" element={<OneProduct />} />
+        <Route
+          path="/cart"
+          element={
+            <Basket panier={panier} handleDeletPanier={handleDeletPanier} />
+          }
+        />
+        <Route
+          path="/products/:id"
+          element={<OneProduct handlePanier={handlePanier} />}
+        />
       </Routes>
       <Footer />
     </Router>
