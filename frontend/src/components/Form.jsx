a55b0930a007902d48934d5011474e0b380bd3cb
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2022-11-22 09:07:44 +0100

    deleted modal code lines

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -67,54 +67,10 @@ function Form() {
             <button
               type="submit"
               onClick={() => sendEmail}
-              data-toggle="modal"
-              data-target="#exampleModalCenter"
               className="btn btn-success text-white formSendButton mb-2 contactButton textRegular"
             >
               Send
             </button>
-            <div
-              className="modal fade"
-              id="exampleModalCenter"
-              tabIndex="-1"
-              role="dialog"
-              aria-labelledby="exampleModalCenterTitle"
-              aria-hidden="true"
-            >
-              <div
-                className="modal-dialog modal-dialog-centered"
-                role="document"
-              >
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <h5 className="modal-title" id="exampleModalLongTitle">
-                      Modal title
-                    </h5>
-                    <button
-                      type="button"
-                      className="close"
-                      data-dismiss="modal"
-                      aria-label="Close"
-                    >
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div className="modal-body">...</div>
-                  <div className="modal-footer">
-                    <button
-                      type="button"
-                      className="btn btn-secondary"
-                      data-dismiss="modal"
-                    >
-                      Close
-                    </button>
-                    <button type="button" className="btn btn-primary">
-                      Save changes
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </form>
